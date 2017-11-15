b35e479369e924d7a2236ca4439040203ee9bc7f
Jeriko <jcarrico1985@live.com>
2017-11-15 10:19:18 -0600

    added functionality to take list of specific users posts and show on the view.

--- src/main/java/com/codeup/blog/repositories/PostsRepository.java
+++ src/main/java/com/codeup/blog/repositories/PostsRepository.java
@@ -4,4 +4,5 @@ import com.codeup.blog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostsRepository extends CrudRepository<Post, Long> {
+  Iterable<Post> findAllById(Long id);
 }
