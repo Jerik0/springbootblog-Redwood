38eb29114b90dac51c4fa22d26dc99b19f073236
Jeriko <jcarrico1985@live.com>
2017-11-17 15:37:11 -0600

    moved user links to right side of topbar and enabled shortening of displayed title on post index page.

--- src/main/resources/static/logic.js
+++ src/main/resources/static/logic.js
@@ -2,18 +2,18 @@
 
 (() => {
 
-  const postPanel = $('.post-title');
+  const postTitle = $('.post-title');
 
-  postPanel.hover((e) => {
-    console.log('hover in');
-    // $(e.target).removeClass('post-title').addClass('shown');
-  }, (e) => {
-    console.log('hover out');
-    // $(e.target).removeClass('shown').addClass('post-title');
+  postTitle.each(function() {
+    let thisText = this.text;
+    if(this.text.length > 15) {
+      let replaceString = thisText.substring(18);
+      let newPostTitle = thisText.replace(replaceString, "...");
+      console.log('length is greater than 15!');
+      console.log(replaceString);
+      this.text = newPostTitle;
+      console.log('title manager triggered!');
+    }
   });
-
-  const hover = () => {
-
-  }
 
 })();
