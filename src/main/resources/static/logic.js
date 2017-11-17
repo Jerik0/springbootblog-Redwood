c938387bd3a8b3d4e90291b27fd5fde420f40505
Jeriko <jcarrico1985@live.com>
2017-11-17 16:57:04 -0600

    added comment in logic.js

--- src/main/resources/static/logic.js
+++ src/main/resources/static/logic.js
@@ -4,12 +4,13 @@
 
   const postTitle = $('.post-title');
 
+  //Check each post title. If title is longer than 18 chars, replace remaining chars with "..."
   postTitle.each(function() {
     let thisText = this.text;
     if(this.text.length > 15) {
       let replaceString = thisText.substring(18);
       let newPostTitle = thisText.replace(replaceString, "...");
-      console.log('length is greater than 15!');
+      console.log('length is greater than 18!');
       console.log(replaceString);
       this.text = newPostTitle;
       console.log('title manager triggered!');
