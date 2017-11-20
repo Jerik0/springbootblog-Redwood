ee33f0d454b3743f0ce70c161a16dc780ae2b0ce
Jeriko <jcarrico1985@live.com>
2017-11-20 11:20:54 -0600

    added timestamp to post on post show page. changed location of post delete button. Fixed margin and spacing of footer and body

--- src/main/resources/static/logic.js
+++ src/main/resources/static/logic.js
@@ -3,18 +3,22 @@
 (() => {
 
   const postTitle = $('.post-title');
+  const postTitleTimestamp = $('.post-title-timestamp');
 
   //Check each post title. If title is longer than 18 chars, replace remaining chars with "..."
   postTitle.each(function() {
     let thisText = this.text;
     if(this.text.length > 15) {
-      let replaceString = thisText.substring(18);
-      let newPostTitle = thisText.replace(replaceString, "...");
-      console.log('length is greater than 18!');
-      console.log(replaceString);
-      this.text = newPostTitle;
-      console.log('title manager triggered!');
+      let replaceString = thisText.substring(15);
+      this.text = thisText.replace(replaceString, "...");
     }
+  });
+
+  postTitleTimestamp.each(function() {
+    console.log(this.innerText);
+    let replaceString = this.innerHTML.substring(8);
+    console.log(replaceString);
+    this.innerText = this.innerText.replace(replaceString, "");
   });
 
 })();
