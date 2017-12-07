c07bca2569fa8c84eb5f7d1dc1e56e2a515405e7
Jeriko <jcarrico1985@live.com>
2017-12-07 14:45:01 -0600

    misc minor changes to do with path restructuring

--- src/main/java/com/codeup/blog/controllers/GameController.java
+++ src/main/java/com/codeup/blog/controllers/GameController.java
@@ -1,12 +1,14 @@
 package com.codeup.blog.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class GameController {
 
   @GetMapping("/gamecenter")
   public String gameCenterHome() {
-    return "gamecenter/index";
+    return "/gamecenter/index";
   }
 
 }
