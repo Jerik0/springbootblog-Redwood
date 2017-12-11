e3cd7c97290ba0861908e7eea76e0f4fb86c8d66
Jeriko <jcarrico1985@live.com>
2017-12-11 16:34:11 -0600

    added beginning of new function in tetris.js.

--- src/main/resources/static/JS/Tetris.js
+++ src/main/resources/static/JS/Tetris.js
@@ -4,13 +4,45 @@
 
   const screen = $('#screen');
 
-  //TODO Create animation for powering on
+
   //TODO Create shape objects (?)
+
+  //TODO Create animation for powering on
+  const powerOn = () => {
+
+  };
+
   //TODO Function for generating shapes
+  const shapeGenerator = () => {
+
+  };
+
   //TODO Function for changing direction of shapes
+  const rotateShape = () => {
+
+  };
+
   //TODO Function for dropping shapes and stopping in the right place
+  const shapeDropper = () => {
+
+  };
+
   //TODO Function for generating points
+  const pointGenerator = () => {
+
+  };
+
   //TODO Function for Win/lose
+  const decideOutcome = () => {
+
+  };
   //TODO Function for detecting when a line is made and disappearing the rows.
+  const nextLevel = () => {
+
+  }
+
+  //TODO Function for speeding up levels as progress is made.
+
+
 
 })();
