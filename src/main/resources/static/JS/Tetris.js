688e3683a13fcca4206b1ac919e3f9aca932a404
Jeriko <jcarrico1985@live.com>
2017-12-11 16:08:40 -0600

    added todo list items in tetris.js. changed minor styling to gameboy structure. on gamecenter index page.

--- src/main/resources/static/JS/Tetris.js
+++ src/main/resources/static/JS/Tetris.js
@@ -4,5 +4,13 @@
 
   const screen = $('#screen');
 
+  //TODO Create animation for powering on
+  //TODO Create shape objects (?)
+  //TODO Function for generating shapes
+  //TODO Function for changing direction of shapes
+  //TODO Function for dropping shapes and stopping in the right place
+  //TODO Function for generating points
+  //TODO Function for Win/lose
+  //TODO Function for detecting when a line is made and disappearing the rows.
 
 })();
