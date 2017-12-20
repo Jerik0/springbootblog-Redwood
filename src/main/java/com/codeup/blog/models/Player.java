a413865998563ee2cea38bb54d3d34a2b9e8ca21
Jeriko <jcarrico1985@live.com>
2017-12-20 14:16:01 -0600

    created player model to add to db

---
+++ src/main/java/com/codeup/blog/models/Player.java
@@ -0,0 +1,51 @@
+package com.codeup.blog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Player {
+
+  @Column
+  private Long id;
+
+  @Column
+  private long highScore;
+
+  @Column
+  private String playername;
+
+  public Player() {
+
+  }
+
+  public Player(Long id, long highScore, String playername) {
+    this.id = id;
+    this.highScore = highScore;
+    this.playername = playername;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public long getHighScore() {
+    return highScore;
+  }
+
+  public void setHighScore(long highScore) {
+    this.highScore = highScore;
+  }
+
+  public String getPlayername() {
+    return playername;
+  }
+
+  public void setPlayername(String playername) {
+    this.playername = playername;
+  }
+}
